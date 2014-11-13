d83c502c1629157b677e7ca182c2cf8ca2a549e2
Brandon Gavett <begavett@yahoo.com>
2014-11-12 17:44:45 -0700

    Formatting changes

--- Lab 11 Code.R
+++ Lab 11 Code.R
@@ -58,41 +58,60 @@ contrasts(hrs.data$Simple) <- simple.effects
 
 
 # From pages 4-5
-I.2x2.1 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Once a week") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.2 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Two or three times a month") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.3 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "One or more times a year") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.4 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Not at all") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.5 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Once a week") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.6 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Two or three times a month") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.7 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "One or more times a year") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.8 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Not at all") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.9 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Once a week") & (hrs.data$Importance == "Somewhat important" | hrs.data$Importance == "Very important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.10 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Two or three times a month") & (hrs.data$Importance == "Somewhat important" | hrs.data$Importance == "Very important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.11 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "One or more times a year") & (hrs.data$Importance == "Somewhat important" | hrs.data$Importance == "Very important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.12 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Not at all") & (hrs.data$Importance == "Somewhat important" | hrs.data$Importance == "Very important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.13 <- with(hrs.data[(hrs.data$FreqRS == "Once a week" | hrs.data$FreqRS == "Two or three times a month") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.14 <- with(hrs.data[(hrs.data$FreqRS == "Once a week" | hrs.data$FreqRS == "One or more times a year") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.15 <- with(hrs.data[(hrs.data$FreqRS == "Once a week" | hrs.data$FreqRS == "Not at all") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.16 <- with(hrs.data[(hrs.data$FreqRS == "Two or three times a month" | hrs.data$FreqRS == "One or more times a year") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.17 <- with(hrs.data[(hrs.data$FreqRS == "Two or three times a month" | hrs.data$FreqRS == "Not at all") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], aov(MNSSCORE ~ FreqRS*Importance))
-
-I.2x2.18 <- with(hrs.data[(hrs.data$FreqRS == "One or more times a year" | hrs.data$FreqRS == "Not at all") & (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.1 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Once a week") & 
+                           (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], 
+                aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.2 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Two or three times a month") & 
+                           (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], 
+                aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.3 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "One or more times a year") & 
+                           (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], 
+                aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.4 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Not at all") & 
+                           (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], 
+                aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.5 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Once a week") & 
+                           (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], 
+                aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.6 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Two or three times a month") & 
+                           (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], 
+                aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.7 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "One or more times a year") & 
+                           (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], 
+                aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.8 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Not at all") & 
+                           (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], 
+                aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.9 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Once a week") & 
+                           (hrs.data$Importance == "Somewhat important" | hrs.data$Importance == "Very important"),], 
+                aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.10 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Two or three times a month") & 
+                            (hrs.data$Importance == "Somewhat important" | hrs.data$Importance == "Very important"),], 
+                 aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.11 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "One or more times a year") & 
+                            (hrs.data$Importance == "Somewhat important" | hrs.data$Importance == "Very important"),], 
+                 aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.12 <- with(hrs.data[(hrs.data$FreqRS == "More than once a week" | hrs.data$FreqRS == "Not at all") & 
+                            (hrs.data$Importance == "Somewhat important" | hrs.data$Importance == "Very important"),], 
+                 aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.13 <- with(hrs.data[(hrs.data$FreqRS == "Once a week" | hrs.data$FreqRS == "Two or three times a month") & 
+                            (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], 
+                 aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.14 <- with(hrs.data[(hrs.data$FreqRS == "Once a week" | hrs.data$FreqRS == "One or more times a year") & 
+                            (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], 
+                 aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.15 <- with(hrs.data[(hrs.data$FreqRS == "Once a week" | hrs.data$FreqRS == "Not at all") & 
+                            (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], 
+                 aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.16 <- with(hrs.data[(hrs.data$FreqRS == "Two or three times a month" | hrs.data$FreqRS == "One or more times a year") & 
+                            (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Somewhat important"),], 
+                 aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.17 <- with(hrs.data[(hrs.data$FreqRS == "Two or three times a month" | hrs.data$FreqRS == "Not at all") & 
+                            (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], 
+                 aov(MNSSCORE ~ FreqRS*Importance))
+I.2x2.18 <- with(hrs.data[(hrs.data$FreqRS == "One or more times a year" | hrs.data$FreqRS == "Not at all") & 
+                            (hrs.data$Importance == "Not too important" | hrs.data$Importance == "Very important"),], 
+                 aov(MNSSCORE ~ FreqRS*Importance))
 
 summary(I.2x2.1)
 summary(I.2x2.2)
